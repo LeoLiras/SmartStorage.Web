eafef883a62cbd669935155974a1c37259378d64
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2025-10-29 12:17:13 -0300

    Mapping Endpoints

--- SmartStorage-API/SmartStorage.Blazor/Utils/ApiExtensions.cs
+++ SmartStorage-API/SmartStorage.Blazor/Utils/ApiExtensions.cs
@@ -1,15 +1,43 @@
-﻿using System.Net.Http.Json;
+﻿using SmartStorage_Shared.VO;
+using System.Net.Http.Json;
 
 namespace SmartStorage.Blazor.Utils
 {
+    
     public class ApiExtensions
     {
+        #region Properties
+
         private readonly HttpClient _http;
+
+        #endregion
+
+        #region Constructros
 
         public ApiExtensions(HttpClient http)
         {
             _http = http;
         }
+
+        #endregion
+
+        #region Endpoints
+
+        //Produtos em estoque
+        private string productsEndpoint = "api/storage/products/v1";
+
+        private string shelvesEndpoint = "api/storage/shelf/v1";
+
+        //Produtos alocados para venda (nas prateleiras)
+        private string entersEndpoint = "api/storage/shelf/v1/allocation";
+
+        private string salesEndpoint = "api/storage/sales/v1/1";
+
+        private string employeesEndpoint = "api/storage/employees/v1";
+
+        #endregion
+
+        #region Methods
 
         /// <summary>
         /// Requisição GET que retorna uma lista do modelos
@@ -19,14 +47,16 @@ namespace SmartStorage.Blazor.Utils
         /// <param name="url"></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public async Task<List<T>?> Get<TVO, T>(string url) where TVO : class
+        public async Task<List<T>?> Get<TVO, T>() where TVO : class
         {
-            if(string.IsNullOrWhiteSpace(url))
+            var url = ReturnEndpoint<TVO>(ERequestTypes.GETAll);
+
+            if (string.IsNullOrWhiteSpace(url))
                 throw new ArgumentNullException(nameof(url), message: "O campo URL é obrigatório.");
 
             var voList = await _http.GetFromJsonAsync<List<TVO>>(url);
 
-            if (voList == null) 
+            if (voList == null)
                 return null;
 
             var parseListMethod = typeof(TVO).GetMethod("ParseList", new[] { typeof(List<TVO>) });
@@ -45,8 +75,10 @@ namespace SmartStorage.Blazor.Utils
         /// <typeparam name="TVO"></typeparam>
         /// <param name="url"></param>
         /// <returns></returns>
-        public async Task<List<TVO>?> Get<TVO>(string url) where TVO : class
+        public async Task<List<TVO>?> Get<TVO>() where TVO : class
         {
+            var url = ReturnEndpoint<TVO>(ERequestTypes.GETAll);
+
             if (string.IsNullOrWhiteSpace(url))
                 throw new ArgumentNullException(nameof(url), message: "O campo URL é obrigatório.");
 
@@ -67,8 +99,10 @@ namespace SmartStorage.Blazor.Utils
         /// <param name="id"></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public async Task<T?> GetWithId<TVO, T>(string url, int id) where TVO : class
+        public async Task<T?> GetById<TVO, T>(int id) where TVO : class
         {
+            var url = ReturnEndpoint<TVO>(ERequestTypes.GetId);
+
             if (string.IsNullOrWhiteSpace(url))
                 throw new ArgumentNullException(nameof(url), message: "O campo URL é obrigatório.");
 
@@ -99,8 +133,10 @@ namespace SmartStorage.Blazor.Utils
         /// <param name="id"></param>
         /// <returns></returns>
         /// <exception cref="InvalidOperationException"></exception>
-        public async Task<TVO?> GetWithId<TVO>(string url, int id) where TVO : class
+        public async Task<TVO?> GetById<TVO>(int id) where TVO : class
         {
+            var url = ReturnEndpoint<TVO>(ERequestTypes.GetId);
+
             if (string.IsNullOrWhiteSpace(url))
                 throw new ArgumentNullException(nameof(url), message: "O campo URL é obrigatório.");
 
@@ -123,16 +159,18 @@ namespace SmartStorage.Blazor.Utils
         /// <param name="vo"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public async Task<TVO?> Post<TVO>(string url, TVO vo) where TVO : class
+        public async Task<TVO?> Post<TVO>(TVO vo) where TVO : class
         {
+            var url = ReturnEndpoint<TVO>(ERequestTypes.POST);
+
             if (vo == null)
                 throw new ArgumentNullException(nameof(vo), message: "O parâmetro VO é obrigatório.");
 
-            if(string.IsNullOrWhiteSpace(url))
+            if (string.IsNullOrWhiteSpace(url))
                 throw new ArgumentNullException(nameof(url), message: "O parâmetro URL é obrigatório.");
 
             var response = await _http.PostAsJsonAsync(url, vo);
-            
+
             response.EnsureSuccessStatusCode();
 
             var result = await response.Content.ReadFromJsonAsync<TVO>();
@@ -148,8 +186,10 @@ namespace SmartStorage.Blazor.Utils
         /// <param name="vo"></param>
         /// <returns></returns>
         /// <exception cref="ArgumentNullException"></exception>
-        public async Task<TVO?> Put<TVO>(string url, TVO vo) where TVO : class
+        public async Task<TVO?> Put<TVO>(TVO vo) where TVO : class
         {
+            var url = ReturnEndpoint<TVO>(ERequestTypes.PUT);
+
             if (vo == null)
                 throw new ArgumentNullException(nameof(vo), message: "O parâmetro VO é obrigatório.");
 
@@ -165,6 +205,42 @@ namespace SmartStorage.Blazor.Utils
             return result;
         }
 
+        private string ReturnEndpoint<TVO>(ERequestTypes type) where TVO : class
+        {
+            if (typeof(TVO) == typeof(EmployeeVO))
+                return employeesEndpoint;
+
+            else if (typeof(TVO) == typeof(EnterVO))
+                return entersEndpoint;
+
+            else if (typeof(TVO) == typeof(ProductVO))
+                return productsEndpoint;
+
+            else if (typeof(TVO) == typeof(SaleVO))
+                return salesEndpoint;
+
+            else if (typeof(TVO) == typeof(ShelfVO))
+                return shelvesEndpoint;
+
+            else
+                return string.Empty;
+        }
+
+        #endregion
+
+        #region Enums
+
+        private enum ERequestTypes
+        {
+            GETAll = 0,
+            GetId,
+            POST,
+            PUT,
+            PATCH,
+            DELETE
+        }
+
+        #endregion
     }
 
 }
