c48c27aeb20497beb4ab7e62b73aee7f8a345192
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2025-11-08 20:18:29 -0300

    Implement employees loading at initialization

--- SmartStorage-API/SmartStorage.Blazor/Utils/VariablesExtensions.cs
+++ SmartStorage-API/SmartStorage.Blazor/Utils/VariablesExtensions.cs
@@ -4,6 +4,8 @@ namespace SmartStorage.Blazor.Utils
 {
     public class VariablesExtensions
     {
-        public ProductVO? ActualProduct { get; set; } = new ProductVO();
+        public ProductVO? ActualProduct { get; set; }
+
+        public List<EmployeeVO?> Employees { get; set; }
     }
 }
