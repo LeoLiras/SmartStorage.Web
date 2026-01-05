13672808baa273d6ca8582361736a0a7c1981a83
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-05 15:43:55 -0300

    Implement chat

---
+++ SmartStorage-API/SmartStorage.Blazor/Utils/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazor;
+
+namespace SmartStorage.Blazor.Utils
+{
+    public class ChatMessage
+    {
+        public string Text { get; set; } = "";
+        public bool IsUser { get; set; }
+        public ChatBubblePosition Position => IsUser ? ChatBubblePosition.End : ChatBubblePosition.Start;
+    }
+}
