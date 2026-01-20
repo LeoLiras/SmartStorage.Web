533d3c361442098407c22bacc633acd234871ef5
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-20 16:26:49 -0300

    Implement PasswordHasher

---
+++ SmartStorage-API/SmartStorage-API/Authentication/Contract/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartStorage_API.Authentication.Contract.Tools
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string password, string hashedPassword);
+    }
+}
