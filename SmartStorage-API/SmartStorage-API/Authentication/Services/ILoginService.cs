33c1777881c18f32715bf6abdc933fa7c0391065
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-21 11:38:32 -0300

    Implement Login Service

---
+++ SmartStorage-API/SmartStorage-API/Authentication/Services/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using SmartStorage_API.Authentication.DTO_s;
+
+namespace SmartStorage_API.Authentication.Services
+{
+    public interface ILoginService
+    {
+        TokenDTO? ValidateCredentials(UserDTO user);
+        TokenDTO? ValidateCredentials(TokenDTO token);
+        bool RevokeToken(string username);
+        AccountCredentialsDTO Create(AccountCredentialsDTO user);
+    }
+}
