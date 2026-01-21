90da80262535f6fec65f2dafd6f9d0965cbdc781
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-21 10:32:36 -0300

    Implement TokenGenerator

---
+++ SmartStorage-API/SmartStorage-API/Authentication/Contract/ITokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace SmartStorage_API.Authentication.Contract
+{
+    public interface ITokenGenerator
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
