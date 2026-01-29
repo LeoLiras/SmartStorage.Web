d6b7b352078a00c995528543de149e776cd1a863
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-29 12:04:36 -0300

    Implement AuthStateProvider class

---
+++ SmartStorage-API/SmartStorage.Blazor/Auth/AuthStateProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace SmartStorage.Blazor.Auth
+{
+    public class AuthStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(4000);
+            
+            var usuario = new ClaimsIdentity("demo");
+
+            return await Task.FromResult(new AuthenticationState(
+                new ClaimsPrincipal(usuario)));
+
+        }
+    }
+}
