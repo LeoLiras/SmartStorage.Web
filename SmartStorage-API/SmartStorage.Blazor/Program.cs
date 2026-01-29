9c7399468999063e14b55ae68350fb2e356fe72d
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-01-29 13:03:18 -0300

    Implement Authorize View

--- SmartStorage-API/SmartStorage.Blazor/Program.cs
+++ SmartStorage-API/SmartStorage.Blazor/Program.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using SmartStorage.Blazor;
+using SmartStorage.Blazor.Auth;
 using SmartStorage.Blazor.Utils;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,10 +12,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5208/") });
 
+builder.Services.AddAuthorizationCore();
+
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<ApiExtensions>();
 builder.Services.AddScoped<ShowDialog>();
 builder.Services.AddScoped<VariablesExtensions>();
+
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
