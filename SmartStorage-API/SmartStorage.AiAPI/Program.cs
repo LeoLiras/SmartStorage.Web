c33dec1d66d53f164261e8db811b02606e3c346f
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-08 16:36:28 -0300

    Recreate AI Project

--- SmartStorage-API/SmartStorage.AiAPI/Program.cs
+++ SmartStorage-API/SmartStorage.AiAPI/Program.cs
@@ -1,14 +1,21 @@
+using SmartStorage.AIAPI.Utils;
+using SmartStorage.Shared.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSwagger(Utils.apiName, Utils.apiDescription, Utils.apiVersion);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.UseApiDefaults(Utils.apiName, Utils.apiVersion);
 
 app.UseAuthorization();
 
---
+++ SmartStorage-API/SmartStorage.AiAPI/Utils/Utils.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartStorage.AIAPI.Utils
+{
+    public class Utils
+    {
+        public const string apiVersion = "1";
+        public const string apiName = "SmartStorage AI Service";
+        public const string apiDescription = "Serviço do SmartStorage análise de dados com IA.";
+    }
+}
