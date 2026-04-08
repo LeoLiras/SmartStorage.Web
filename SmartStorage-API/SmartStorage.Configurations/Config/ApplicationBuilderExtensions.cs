d4ac7a1a5af9f3f362717c3a93c92cc851d8318c
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-08 16:19:06 -0300

    Implement config project

---
+++ SmartStorage-API/SmartStorage.Configurations/Config/ApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Rewrite;
+
+namespace SmartStorage.Shared.Config
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseApiDefaults(this IApplicationBuilder app, string apiName, string apiVersion)
+        {
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint($"/swagger/{apiVersion}/swagger.json",
+                    $"{apiName} - {apiVersion}");
+            });
+
+            var options = new RewriteOptions();
+            options.AddRedirect("^$", "swagger");
+
+            app.UseRewriter(options);
+
+            return app;
+        }
+    }
+}
---
+++ SmartStorage-API/SmartStorage.Configurations/Config/SwaggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+
+namespace SmartStorage.Shared.Config
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services, string apiName, string apiDescription, string apiVersion)
+        {
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(apiVersion,
+                    new OpenApiInfo
+                    {
+                        Title = apiName,
+                        Version = apiVersion,
+                        Description = apiDescription,
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Leonardo de Lira Siqueira",
+                            Url = new Uri("https://github.com/LeoLiras")
+                        }
+                    });
+            });
+
+            return services;
+        }
+    }
+}
