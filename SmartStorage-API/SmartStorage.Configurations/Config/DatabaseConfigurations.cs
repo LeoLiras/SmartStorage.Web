464fc0bca876f8497538fe83bc2863a168d9de6d
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-09 12:55:19 -0300

    Implement Database Configuration

---
+++ SmartStorage-API/SmartStorage.Configurations/Config/DatabaseConfigurations.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartStorage_API.Model.Context;
+
+namespace SmartStorage.Configurations.Config
+{
+    public static class DatabaseConfigurations
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<SmartStorageContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SqlServerConnection"), b => b.MigrationsAssembly(typeof(SmartStorageContext).Assembly.GetName().Name)));
+
+            return services;
+        }
+    }
+}
