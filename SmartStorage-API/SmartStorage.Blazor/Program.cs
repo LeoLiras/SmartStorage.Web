1a521189446fdda8650c625cf5705dd4dc60e494
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-09 11:42:52 -0300

    Config ports

--- SmartStorage-API/SmartStorage.Blazor/Program.cs
+++ SmartStorage-API/SmartStorage.Blazor/Program.cs
@@ -12,7 +12,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5208/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5101/") });
 
 builder.Services.AddAuthorizationCore();
 
