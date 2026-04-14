440f0976fb45eee9bbf3d037341480af3cd48f27
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-14 12:37:21 -0300

    Delete unnecessary class

--- SmartStorage-API/SmartStorage.Shared/Utils/Version.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SmartStorage.Shared.Utils
-{
-    public static class Version
-    {
-        public const string apiVersion = "1";
-        public const string apiName = "SmartStorage API";
-        public const string apiDescription = "API do SmartStorage para fazer as principais operações CRUD.";
-    }
-}
