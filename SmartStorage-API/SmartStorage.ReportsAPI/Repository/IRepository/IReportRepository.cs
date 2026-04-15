ba0fdb9409056d72ed798e14d9bc2545d0850be5
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-15 16:18:39 -0300

    Implement Report IRepository

---
+++ SmartStorage-API/SmartStorage.ReportsAPI/Repository/IRepository/IReportRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartStorage.ReportsAPI.Repository.IRepository
+{
+    public interface IReportRepository
+    {
+        byte[] GenerateExcel();
+        Task<byte[]> GeneratePdf();
+    }
+}
