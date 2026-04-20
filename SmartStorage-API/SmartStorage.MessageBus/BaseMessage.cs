515e8d105e1c0d0ec7cdeca0358ffde9092a1e60
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-20 13:10:24 -0300

    Create MessageBus project

---
+++ SmartStorage-API/SmartStorage.MessageBus/BaseMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartStorage.MessageBus
+{
+    public class BaseMessage
+    {
+        public long Id { get; set; }
+        public DateTime MessageCreated { get; set; }
+    }
+}
---
+++ SmartStorage-API/SmartStorage.MessageBus/IMessageBus.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartStorage.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task PublicMessage(BaseMessage message, string queueName);
+    }
+}
