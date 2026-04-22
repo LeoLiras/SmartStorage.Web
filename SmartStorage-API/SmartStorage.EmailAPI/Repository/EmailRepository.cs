1f38da64e7ee062da50f1122b3d7d558e8501d17
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-22 12:58:23 -0300

    Create Email repository

---
+++ SmartStorage-API/SmartStorage.EmailAPI/Repository/EmailRepository.cs
@@ -0,0 +1,13 @@
+﻿using SmartStorage.EmailAPI.Repository.Interfaces;
+using SmartStorage_Shared.VO;
+
+namespace SmartStorage.EmailAPI.Repository
+{
+    public class EmailRepository : IEmailRepository
+    {
+        public Task NewProductEmail(ProductVO product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SmartStorage-API/SmartStorage.EmailAPI/Repository/Interfaces/IEmailRepository.cs
@@ -0,0 +1,9 @@
+﻿using SmartStorage_Shared.VO;
+
+namespace SmartStorage.EmailAPI.Repository.Interfaces
+{
+    public interface IEmailRepository
+    {
+        Task NewProductEmail(ProductVO product);
+    }
+}
