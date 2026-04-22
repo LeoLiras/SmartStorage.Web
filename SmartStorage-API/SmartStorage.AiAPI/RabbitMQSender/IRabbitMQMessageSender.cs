cb1d078bd4693147cd363b1b794bae4aa762900f
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-22 12:48:37 -0300

    Delete RabbitMQ files

--- SmartStorage-API/SmartStorage.AiAPI/RabbitMQSender/IRabbitMQMessageSender.cs
+++
@@ -1,9 +0,0 @@
-﻿using SmartStorage.MessageBus;
-
-namespace SmartStorage.AIAPI.RabbitMQSender
-{
-    public interface IRabbitMQMessageSender
-    {
-        void SendMessage(BaseMessage baseMessage, string queueName);
-    }
-}
