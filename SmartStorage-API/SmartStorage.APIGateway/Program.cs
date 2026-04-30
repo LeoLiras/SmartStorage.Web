395e4798a4a03a7bd9b29c1391d2b8fdddc6d7df
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-04-30 11:51:17 -0300

    Implement ocelot config to ReportsAPI

--- SmartStorage-API/SmartStorage.APIGateway/Program.cs
+++ SmartStorage-API/SmartStorage.APIGateway/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using SmartStorage.Configurations.Config;
+using SmartStorage.Shared.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddPolicyConfig("Blazor", ["http://localhost:5001"]);
+
 builder.Services.AddOcelot();
 
 var app = builder.Build();
+
+app.UseCors("Blazor");
 
 // Configure the HTTP request pipeline.
 
