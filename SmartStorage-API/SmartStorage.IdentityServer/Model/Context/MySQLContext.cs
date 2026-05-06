2aac64ea98f3cd15d0c8a86198bdc4a0e6675ecf
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-05-06 11:59:54 -0300

    Confg database and context to IdentityServer

---
+++ SmartStorage-API/SmartStorage.IdentityServer/Model/Context/MySQLContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartStorage.IdentityServer.Model.Context
+{
+    public class MySQLContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions<MySQLContext> options)
+            : base(options) { }
+    }
+}
