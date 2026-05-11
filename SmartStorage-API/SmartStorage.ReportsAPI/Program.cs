00294b34ee263a451165772be28ef5d189860f44
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-05-11 13:22:32 -0300

    Config Report Auth

--- SmartStorage-API/SmartStorage.ReportsAPI/Program.cs
+++ SmartStorage-API/SmartStorage.ReportsAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.IdentityModel.Tokens;
 using QuestPDF.Infrastructure;
 using SmartStorage.Configurations.Config;
 using SmartStorage.ReportsAPI.Repository;
@@ -21,6 +22,26 @@ builder.Services.AddPolicyConfig("Blazor", ["https://localhost:4480"]);
 
 builder.Services.AddScoped<IReportRepository, ReportRepository>();
 
+builder.Services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    options.Authority = "https://localhost:5200/";
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateAudience = false
+                    };
+                });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "smart_storage");
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +49,8 @@ var app = builder.Build();
 app.UseApiDefaults(Utils.apiName, Utils.apiVersion, "Blazor");
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
