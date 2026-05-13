3bfe100282ce6abd41e9044659441cb923dee777
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-05-13 13:25:57 -0300

    Config Auth Project Program.cs

--- SmartStorage-API/SmartStorage.AuthenticationAPI/Program.cs
+++ SmartStorage-API/SmartStorage.AuthenticationAPI/Program.cs
@@ -1,8 +1,24 @@
+using SmartStorage.AuthenticationAPI.Config;
+using SmartStorage.AuthenticationAPI.Contract;
+using SmartStorage.AuthenticationAPI.Contract.Tools;
+using SmartStorage.AuthenticationAPI.Repositories;
+using SmartStorage.AuthenticationAPI.Repositories.Implementations;
+using SmartStorage.AuthenticationAPI.Services;
+using SmartStorage.AuthenticationAPI.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthConfiguration(builder.Configuration);
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IPasswordHasher, Sha256PasswordHasher>();
+builder.Services.AddScoped<IUserAuthService, UserAuthServiceImplementation>();
+builder.Services.AddScoped<ILoginService, LoginServiceImplementation>();
+builder.Services.AddScoped<ITokenGenerator, TokenGenerator>();
 
 var app = builder.Build();
 
