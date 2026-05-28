f62d5629148f0b850b8d9604a360b04375875529
Leonardo de Lira <leonardo018.siqueira@hotmail.com>
2026-05-28 12:24:48 -0300

    Implement Auth header handler

---
+++ SmartStorage-API/SmartStorage.Blazor/Authentication/AuthHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.JSInterop;
+using System.Net.Http.Headers;
+
+namespace SmartStorage.Blazor.Authentication
+{
+    public class AuthHandler : DelegatingHandler
+    {
+        private readonly IJSRuntime js;
+
+        public AuthHandler(IJSRuntime jsRuntime)
+        {
+            js = jsRuntime;
+        }
+
+        protected override async Task<HttpResponseMessage>SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await js.InvokeAsync<string>(
+                "localStorage.getItem",
+                "tokenKey");
+
+            if (!string.IsNullOrWhiteSpace(token))
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
